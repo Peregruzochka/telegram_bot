a1cc8b3f26bc4a7a12175c9758d3b7fba231d62b
Peregruzochka <peregruzochka48@mail.ru>
2024-10-30 20:45:05 +0300

    информация в зависимости от запроса в базу

---
+++ src/main/java/ru/pereguzochka/telegram_bot/dto/RegistrationDto.java
@@ -0,0 +1,33 @@
+package ru.pereguzochka.telegram_bot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationDto {
+    private Long telegramId;
+    private String username;
+    private UUID lessonId;
+    private UUID teacherId;
+    private RegistrationType type;
+    private UUID slotId;
+    private String childrenName;
+    private int childrenYear;
+    private int childrenMonth;
+    private String phone;
+
+    public enum RegistrationType {
+        NEW_USER,
+        REGULAR_USER,
+        RE_REGISTRATION
+    }
+}
--- src/main/java/ru/pereguzochka/telegram_bot/handler/start_handler/FirstStartAttribute.java
+++ src/main/java/ru/pereguzochka/telegram_bot/handler/start_handler/FirstStartAttribute.java
@@ -5,10 +5,7 @@ import org.springframework.stereotype.Component;
 import ru.pereguzochka.telegram_bot.handler.BaseAttribute;
 
 @Component
-@ConfigurationProperties(prefix = "attr.start")
-public class StartAttribute extends BaseAttribute {
+@ConfigurationProperties(prefix = "attr.first-start")
+public class FirstStartAttribute extends BaseAttribute {
 
-    public String createText(String name) {
-        return text.replace("{}", name);
-    }
 }
