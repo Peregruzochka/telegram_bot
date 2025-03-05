fa7d2aed538cb89fbc1b434ec8252a5b1ded8288
Peregruzochka <peregruzochka48@mail.ru>
2025-03-06 00:05:05 +0300

    создание prod и test профилей

--- src/test/java/ru/pereguzochka/telegram_bot/TelegramBotApplicationTests.java
+++ src/test/java/ru/pereguzochka/telegram_bot/TelegramBotApplicationTests.java
@@ -2,9 +2,8 @@ package ru.pereguzochka.telegram_bot;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TelegramBotApplicationTests {
 
 	@Test
