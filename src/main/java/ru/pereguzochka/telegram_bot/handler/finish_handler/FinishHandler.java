fae51c5852c6d5286f1adfedffd6e2ee63d7a3fa
Peregruzochka <peregruzochka48@mail.ru>
2025-03-16 19:34:54 +0300

    добавил обработку исключения, когда несколько клиентов пытаются записаться на одно и тоже время

--- src/main/java/ru/pereguzochka/telegram_bot/handler/finish_handler/FinishHandler.java
+++ src/main/java/ru/pereguzochka/telegram_bot/handler/finish_handler/FinishHandler.java
@@ -1,5 +1,6 @@
 package ru.pereguzochka.telegram_bot.handler.finish_handler;
 
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -11,8 +12,6 @@ import ru.pereguzochka.telegram_bot.dto.TimeSlotDto;
 import ru.pereguzochka.telegram_bot.dto.UserDto;
 import ru.pereguzochka.telegram_bot.handler.UpdateHandler;
 
-import static ru.pereguzochka.telegram_bot.dto.RegistrationDto.RegistrationType.RE_REGISTRATION;
-
 @Component
 @RequiredArgsConstructor
 public class FinishHandler implements UpdateHandler {
@@ -20,6 +19,7 @@ public class FinishHandler implements UpdateHandler {
     private final RegistrationCache registrationCache;
     private final FinishAttribute finishAttribute;
     private final BotBackendClient botBackendClient;
+    private final WrongFinishAttribute wrongFinishAttribute;
 
     @Override
     public boolean isApplicable(Update update) {
@@ -34,14 +34,17 @@ public class FinishHandler implements UpdateHandler {
         UserDto userDto = registrationDto.getUser();
         userDto.setTelegramId(telegramId);
 
-        if (registrationDto.getType().equals(RE_REGISTRATION)) {
-            botBackendClient.updateRegistration(registrationDto);
-        } else {
+        try {
             botBackendClient.addRegistration(registrationDto);
+            registrationCache.remove(telegramId);
+            bot.edit(finishAttribute.generateText(slot), finishAttribute.createMarkup(), update);
+
+        } catch (FeignException.InternalServerError e) {
+            int httpCode = e.status();
+            String responceBody = e.contentUTF8();
+            if (httpCode == 500 && responceBody.contains("TimeSlot is not available")) {
+                bot.edit(wrongFinishAttribute.getText(), wrongFinishAttribute.createMarkup(), update);
+            }
         }
-
-        registrationCache.remove(telegramId);
-
-        bot.edit(finishAttribute.generateText(slot), finishAttribute.createMarkup(), update);
     }
 }
