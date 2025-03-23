79b2f2722ac6cca1d2eba3eff0ea5ea3436ec6bf
Peregruzochka <peregruzochka48@mail.ru>
2025-03-23 14:50:25 +0300

    refactor start handler

--- src/main/java/ru/pereguzochka/telegram_bot/dto/UserDto.java
+++ src/main/java/ru/pereguzochka/telegram_bot/dto/UserDto.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,10 +16,21 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
-public class UserDto {
+@ToString
+public class UserDto implements Serializable {
     private UUID id;
     private Long telegramId;
     private String name;
     private String phone;
+    private UserStatus status;
     private List<ChildDto> children;
+
+
+    public enum UserStatus {
+        NEW,
+        REGULAR,
+        EDITING,
+    }
 }
+
+
--- src/main/java/ru/pereguzochka/telegram_bot/handler/start_handler/StartMessageHandler.java
+++ src/main/java/ru/pereguzochka/telegram_bot/handler/start_handler/StartMessageHandler.java
@@ -1,29 +1,29 @@
 package ru.pereguzochka.telegram_bot.handler.start_handler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.pereguzochka.telegram_bot.bot.TelegramBot;
-import ru.pereguzochka.telegram_bot.cache.RegistrationCache;
-import ru.pereguzochka.telegram_bot.cache.UserDtoCache;
 import ru.pereguzochka.telegram_bot.client.BotBackendClient;
-import ru.pereguzochka.telegram_bot.dto.RegistrationDto;
 import ru.pereguzochka.telegram_bot.dto.UserDto;
 import ru.pereguzochka.telegram_bot.handler.UpdateHandler;
+import ru.pereguzochka.telegram_bot.redis.redis_repository.UsersByTelegramId;
 
-import static ru.pereguzochka.telegram_bot.dto.RegistrationDto.RegistrationType.NEW_USER;
-import static ru.pereguzochka.telegram_bot.dto.RegistrationDto.RegistrationType.REGULAR_USER;
+import static ru.pereguzochka.telegram_bot.dto.UserDto.UserStatus.NEW;
+import static ru.pereguzochka.telegram_bot.dto.UserDto.UserStatus.REGULAR;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StartMessageHandler implements UpdateHandler {
 
     private final FirstStartAttribute firstStartAttribute;
     private final StartAttribute startAttribute;
-    private final TelegramBot bot;
-    private final RegistrationCache cache;
     private final BotBackendClient botBackendClient;
-    private final UserDtoCache userDtoCache;
+
+    private final UsersByTelegramId usersByTelegramId;
+    private final TelegramBot telegramBot;
 
     public boolean isApplicable(Update update) {
         return update.hasMessage() && update.getMessage().getText().equals("/start");
@@ -33,27 +33,20 @@ public class StartMessageHandler implements UpdateHandler {
         Long telegramId = update.getMessage().getFrom().getId();
         UserDto userDto = botBackendClient.getUserByTelegramId(telegramId);
         if (userDto == null) {
-            cache.put(telegramId, createRegistrationDtoForNewUser(telegramId));
-            bot.send(firstStartAttribute.getText(), firstStartAttribute.createMarkup(), update);
-        } else {
-            cache.put(telegramId, createRegistrationDtoForRegularUser(userDto));
-            userDtoCache.put(telegramId, userDto);
-            bot.send(startAttribute.createText(userDto.getName()), startAttribute.createMarkup(), update);
+            userDto = new UserDto();
+            userDto.setTelegramId(telegramId);
+            userDto.setStatus(NEW);
         }
-    }
 
-    private RegistrationDto createRegistrationDtoForNewUser(Long telegramId) {
-        return RegistrationDto.builder()
-                .telegramId(telegramId)
-                .type(NEW_USER)
-                .build();
-    }
+        usersByTelegramId.put(telegramId.toString(), userDto);
 
-    private RegistrationDto createRegistrationDtoForRegularUser(UserDto userDto) {
-        return RegistrationDto.builder()
-                .telegramId(userDto.getTelegramId())
-                .user(userDto)
-                .type(REGULAR_USER)
-                .build();
+        if (userDto.getStatus() == NEW) {
+            telegramBot.send(firstStartAttribute.getText(), firstStartAttribute.createMarkup(), update);
+        } else if (userDto.getStatus() == REGULAR) {
+            String userName = userDto.getName();
+            telegramBot.send(startAttribute.createText(userName), startAttribute.createMarkup(), update);
+        }
+
+        log.info("telegramId: {} -> /start: {}", telegramId, userDto.getStatus().toString());
     }
 }
