c790904de7950a77a47c317dcfc09fe1d671bc68
Peregruzochka <peregruzochka48@mail.ru>
2025-03-27 22:58:30 +0300

    delete old cache system

--- src/main/java/ru/pereguzochka/telegram_bot/cache/WeekCursorCache.java
+++
@@ -1,9 +0,0 @@
-package ru.pereguzochka.telegram_bot.cache;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class WeekCursorCache extends Cache<Long, Integer> {
-//    Key:      ChatId
-//    Value:    nextWeek
-}
